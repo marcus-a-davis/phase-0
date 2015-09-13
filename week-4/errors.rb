ec74c7a7848bf6706850c8aa19c4f21d068dc6ab
Marcus A. Davis <marcus.a.davis24@gmail.com>
2015-09-12 21:18:50 -0700

    Add errors.rb

---
+++ week-4/errors.rb
@@ -0,0 +1,190 @@
+# Analyze the Errors
+
+# I worked on this challenge [by myself, with: ].
+# I spent [#] hours on this challenge.
+
+# --- error -------------------------------------------------------
+
+# "Screw you guys " + "I'm going home." = cartmans_phrase
+
+# 1. What is the name of the file with the error?
+#    errors.rb
+# 2. What is the line number where the error occurs?
+#    8
+# 3. What is the type of error message?
+#    syntax error
+# 4. What additional information does the interpreter provide about this type of error?
+#    unexpected '=', expecting end of input
+# 5. Where is the error in the code?
+#    After the equals sign
+# 6. Why did the interpreter give you this error?
+#    In Ruby variables have to be the first element identified.
+#    In other words, you would have needed to write that code as
+#    follows: cartmans_phrase = "Screw you guys" + "I'm going home."
+
+# This error was analyzed in the README file.
+# --- error -------------------------------------------------------
+
+def cartman_hates(thing)
+  while true
+    puts "What's there to hate about #{thing}?"
+end
+
+# This is a tricky error. The line number may throw you off.
+
+# 1. What is the name of the file with the error?
+#    errors.rb
+# 2. What is the line number where the error occurs?
+#    172
+# 3. What is the type of error message?
+#    syntax error
+# 4. What additional information does the interpreter provide about this type of error?
+#    unexpected end of input, expecting keyword_end
+# 5. Where is the error in the code?
+#    The error is triggered after the last bit of code in the file.
+#    However, the mistake in the code is that that while loop is not
+#    closed with "end" so that the cartman_hates method is not closed.
+# 6. Why did the interpreter give you this error?
+#    Because there is one less "end" than necessary to close out the
+#    method, the program looks indefinitely for it. Because there
+#    aren't any in the rest of the program, it causes an error.
+#
+
+# --- error -------------------------------------------------------
+
+south_park
+
+# 1. What is the line number where the error occurs?
+#
+# 2. What is the type of error message?
+#
+# 3. What additional information does the interpreter provide about this type of error?
+#
+# 4. Where is the error in the code?
+#
+# 5. Why did the interpreter give you this error?
+#
+
+# --- error -------------------------------------------------------
+
+cartman()
+
+# 1. What is the line number where the error occurs?
+#
+# 2. What is the type of error message?
+#
+# 3. What additional information does the interpreter provide about this type of error?
+#
+# 4. Where is the error in the code?
+#
+# 5. Why did the interpreter give you this error?
+#
+
+# --- error -------------------------------------------------------
+
+def cartmans_phrase
+  puts "I'm not fat; I'm big-boned!"
+end
+
+cartmans_phrase('I hate Kyle')
+
+# 1. What is the line number where the error occurs?
+#
+# 2. What is the type of error message?
+#
+# 3. What additional information does the interpreter provide about this type of error?
+#
+# 4. Where is the error in the code?
+#
+# 5. Why did the interpreter give you this error?
+#
+
+# --- error -------------------------------------------------------
+
+def cartman_says(offensive_message)
+  puts offensive_message
+end
+
+cartman_says
+
+# 1. What is the line number where the error occurs?
+#
+# 2. What is the type of error message?
+#
+# 3. What additional information does the interpreter provide about this type of error?
+#
+# 4. Where is the error in the code?
+#
+# 5. Why did the interpreter give you this error?
+#
+
+
+
+# --- error -------------------------------------------------------
+
+def cartmans_lie(lie, name)
+  puts "#{lie}, #{name}!"
+end
+
+cartmans_lie('A meteor the size of the earth is about to hit Wyoming!')
+
+# 1. What is the line number where the error occurs?
+#
+# 2. What is the type of error message?
+#
+# 3. What additional information does the interpreter provide about this type of error?
+#
+# 4. Where is the error in the code?
+#
+# 5. Why did the interpreter give you this error?
+#
+
+# --- error -------------------------------------------------------
+
+5 * "Respect my authoritay!"
+
+# 1. What is the line number where the error occurs?
+#
+# 2. What is the type of error message?
+#
+# 3. What additional information does the interpreter provide about this type of error?
+#
+# 4. Where is the error in the code?
+#
+# 5. Why did the interpreter give you this error?
+#
+
+# --- error -------------------------------------------------------
+
+amount_of_kfc_left = 20/0
+
+
+# 1. What is the line number where the error occurs?
+#
+# 2. What is the type of error message?
+#
+# 3. What additional information does the interpreter provide about this type of error?
+#
+# 4. Where is the error in the code?
+#
+# 5. Why did the interpreter give you this error?
+#
+
+# --- error -------------------------------------------------------
+
+require_relative "cartmans_essay.md"
+
+# 1. What is the line number where the error occurs?
+#
+# 2. What is the type of error message?
+#
+# 3. What additional information does the interpreter provide about this type of error?
+#
+# 4. Where is the error in the code?
+#
+# 5. Why did the interpreter give you this error?
+#
+
+
+# --- REFLECTION -------------------------------------------------------
+# Write your reflection below as a comment.
